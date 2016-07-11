60d5e9745bc747265a67a2d7036289481a3832a2
queckezz <fabian.eichenberger@gmail.com>
2016-07-11 20:40:31 +0100

    bootstrap with cdn

--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,7 @@
 module View exposing (..)
 
 import Html.Attributes exposing (..)
+import CDN exposing (bootstrap)
 import Html.Events exposing (..)
 import Types exposing (..)
 import Html exposing (..)
@@ -8,7 +9,7 @@ import Html exposing (..)
 root: Model -> Html Msg
 root model =
   div [] [
-    h1 [] [ text "News!" ],
+    h1 [] [bootstrap.css, text "News!" ],
       case model.news of
         Succeed news ->
           ul [] (List.map newsItem news)
